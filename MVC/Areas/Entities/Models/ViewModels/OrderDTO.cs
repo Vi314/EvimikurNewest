516b224ee6b45150892060cb7e684ee98be0c27c
Abdullah Arslan <121155456+Vi314@users.noreply.github.com>
2023-06-06 17:15:07 +0300

    .

--- MVC/Areas/Entities/Models/ViewModels/OrderDTO.cs
+++ MVC/Areas/Entities/Models/ViewModels/OrderDTO.cs
@@ -3,17 +3,23 @@ using System.ComponentModel.DataAnnotations;
 
 namespace MVC.Areas.Entities.Models.ViewModels
 {
-    public class OrderDTO:BaseDTO
+    public class OrderDTO : BaseDTO
     {
         public string DealerName { get; set; } = "";
+
         [Required(ErrorMessage = ErrorMessages.requiredField)]
         public int DealerId { get; set; }
+
         public string EmployeeName { get; set; } = "";
+
         [Required(ErrorMessage = ErrorMessages.requiredField)]
         public int EmployeeId { get; set; }
+
         public string? SupplierName { get; set; }
+
         [Required(ErrorMessage = ErrorMessages.requiredField)]
         public int? SupplierId { get; set; }
+
         public decimal Price { get; set; }
         public DateTime OrderDate { get; set; } = DateTime.Now;
         public OrderType OrderType { get; set; }
