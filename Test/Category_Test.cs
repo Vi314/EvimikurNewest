b3a10becca34b02ca740f0c48ffa5114653c59d0
Abdullah Arslan <121155456+Vi314@users.noreply.github.com>
2023-06-19 09:14:56 +0300

    .

--- Test/Category_Test.cs
+++ Test/Category_Test.cs
@@ -10,15 +10,29 @@ namespace UnitTest;
 
 public class Category_Test
 {
-    
+
     //[Fact]
     //public void Get_All_Method_Must_Return_ListCategory()
     //{
     //    var productList = _service.GetAll().ToList();
-        
+
     //    productList.Should().AllBeOfType<CategoryModel>();
     //    productList.Should().NotBeNull();
     //    productList.Should().BeOfType<List<CategoryModel>>();
 
     //}
+
+    [Fact]
+    public void GetById_Should_Return_CategoryModelType()
+    {
+        // Arrange
+
+
+        // Act
+
+
+        // Assert
+
+
+    }
 }
